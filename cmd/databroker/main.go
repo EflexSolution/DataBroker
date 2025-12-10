076215c2332ee7c444e409abba3e983fd55d59c1
Efle <152645371+EflexSolution@users.noreply.github.com>
2025-12-10 19:24:26 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/databroker/main.go
@@ -0,0 +1,23 @@
+// cmd/databroker/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "databroker/internal/databroker"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := databroker.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
